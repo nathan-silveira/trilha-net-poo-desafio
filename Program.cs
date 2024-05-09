f9fabd77d5da4fc93a1a741a2c99c41e661dcb51
Nathan Silveira <nathansilveira147@gmail.com>
2024-05-09 13:48:15 -0300

    Sistema e abstração de celulares finalizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone cel1 = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+cel1.Ligar();
+cel1.InstalarAplicativo("Facebook");
+
+Smartphone cel2 = new Iphone(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+cel2.Ligar();
+cel2.InstalarAplicativo("Facebook");
